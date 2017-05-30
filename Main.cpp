3304de2ac71c4200bd74c0a5b9ec3069af8bc22d
Adil Malik <619mwa@gmail.com>
2017-05-29 22:23:52 -0600

    Changed to pointers

--- Main.cpp
+++ Main.cpp
@@ -4,44 +4,45 @@
 
 int main() {
 
-	int elements = 5;
+	int elements = 10;
 	int tries = 1000;
 
 	RarityHandler<double> rh;
 
-	std::vector<double> elementList;
+	std::vector<double> elementStorage(elements);
+	std::vector<double*> elementPtrs(elements);
 	std::vector<int> rarities;
 	
-	float counts[100];
+	std::vector<int> counts(elements);
 	for (int i = 1; i <= elements; i++) {
-		elementList.push_back(i);
+		elementStorage[i - 1] = i;
+		elementPtrs[i - 1] = &elementStorage[i - 1];
 		rarities.push_back(i);
 		counts[i - 1] = 0;
 	}
 
-	rh.addList(elementList, rarities);
+	rh.addList(elementPtrs, rarities);
 
 	for (int x = 0; x < tries; x++) {
-		int idx = rh.selectRandomObject();
-		counts[idx]++;
+		int idx = *rh.selectRandomObject();
+		counts[idx - 1]++;
 	}
 
-	std::cout << "1: " << (counts[1] / tries) * 100 << " 5: " << 100 * counts[4] / tries << std::endl;
+	std::cout << "1: " << (counts[0] / (float)tries) * 100 << " 5: " << 100 * counts[elements - 1] / (float)tries << std::endl;
 
 	rh.clear();
 
 	for (int i = 1; i <= elements; i++) {
-		double value = i;
-		rh.addObject(value, i);
+		rh.addObject(elementStorage[i - 1], i);
 		counts[i - 1] = 0;
 	}
 
 	for (int x = 0; x < tries; x++) {
-		int idx = rh.selectRandomObject();
-		counts[idx]++;
+		int idx = *rh.selectRandomObject();
+		counts[idx - 1]++;
 	}
 
-	std::cout << "1: " << (counts[1] / tries) * 100 << " 5: " << 100 * counts[4] / tries << std::endl;
+	std::cout << "1: " << (counts[0] / (float)tries) * 100 << " 5: " << 100 * counts[elements - 1] / (float)tries << std::endl;
 
 	return 0;
 }
